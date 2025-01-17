4e0d4363dec5bd2552cb9648c102d65fb79bd34a
kvishal <vishalkedare97@gmail.com>
2025-01-17 22:55:22 +0530

    Third Commit

--- EmailApplicationProj/src/emailapp/Email.java
+++ EmailApplicationProj/src/emailapp/Email.java
@@ -8,9 +8,11 @@ public class Email {
 	private String lastName;
 	private String department;
 	private String password;
-	private int mailboxcapacity;
+	private int mailboxcapacity=50;
 	private String alternateEmail;
 	private int defaultPasswordLength=10;
+	private String email;
+	private String companySuffix="company.com";
 	
 	// constructor to create firstName and lastName
 	public Email(String firstName,String lastName)
@@ -19,22 +21,22 @@ public class Email {
 		this.lastName=lastName;
 		this.department=setDepartment();
 		this.password=setPassword(defaultPasswordLength);
-		System.out.println("Email Created: "+firstName+" "+lastName);
-		System.out.println("Dept:"+department);
-		System.out.println("Password:"+password);
+		
+		// Generate the Email
+		email = firstName.toLowerCase()+"."+ lastName.toLowerCase()+"@"+department+"."+companySuffix;
 	}
 	
 	// Ask department
 	public String setDepartment()
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Department COde\n1. Sales\n2. dev\n3. acc\nEnter the code:");
+		System.out.println("Department COde\n1 for sales\n2 for dev\n3 for acc\nEnter the code:");
 		int deptCode=sc.nextInt();
 		
 		switch(deptCode)
 		{
 		case 1:
-			return "Sales";
+			return "sales";
 		case 2:
 			return "dev";
 		case 3:
@@ -64,9 +66,40 @@ public class Email {
 	}
 	
 	// Set the mailbox capacity
+	public void setMaiboxCapacity(int capacity)
+	{
+		mailboxcapacity= capacity;
+	}
+	
 	
 	// Set the alternate Email
-	
+	public void setAlternateEmail(String alternateEmail)
+	{
+		this.alternateEmail=alternateEmail;
+	}
 	// Change the password
+	public void setPassword(String password)
+	{
+		this.password=password;
+	}
+	
+	
+	
+	public int getMailboxCapacity() {return mailboxcapacity;}
+	
+	public String getAlternateEmail() {return alternateEmail;}
+	
+	public String getPassword() {return password;} 
+	
+	public void ShowInforrmation()
+	{
+		System.out.println("--------------------------------------");
+		System.out.println("Display Name:"+firstName+" "+lastName);
+		System.out.println("Email: "+email);
+		System.out.println("Generated Password:"+password);
+		System.out.println("Mail Box Capacity: "+mailboxcapacity+"mb");
+		System.out.println("--------------------------------------");
+	}
+	
 	
 }
